2985b2382270ea55d2e79390c468fd43fd270641
Mohammad Maviya Waseem Shaikh <maviya9830@gmail.com>
2023-09-02 23:34:17 +0530

    submit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,58 +2,58 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
+    private int basePrice;
     private boolean isVeg;
     private StringBuilder bill;
+
     private boolean extraCheeseAdded;
     private boolean extraToppingsAdded;
     private boolean paperBagAdded;
 
-    public Pizza(Boolean isVeg){
+    public Pizza(Boolean isVeg) {
         this.isVeg = isVeg;
-        this.price = isVeg ? 300 : 400;
-        this.bill = new StringBuilder("Base Price Of The Pizza: " + price + "\n");
+        this.basePrice = isVeg ? 300 : 400;
+        this.bill = new StringBuilder("Base Price Of The Pizza: " + basePrice + "\n");
+
         this.extraCheeseAdded = false;
         this.extraToppingsAdded = false;
         this.paperBagAdded = false;
     }
 
-    public int getPrice(){
-        return this.price;
+    public int getPrice() {
+        return this.basePrice;
     }
 
-    public void addExtraCheese(){
-        // your code goes here
-        if (!extraCheeseAdded&&price>0) {
-            price += 80;
+    public void addExtraCheese() {
+        if (!extraCheeseAdded) {
+            basePrice += 80;
             bill.append("Extra Cheese Added: 80\n");
             extraCheeseAdded = true;
         }
     }
 
-    public void addExtraToppings(){
-        // your code goes here
-        if (!extraToppingsAdded&&price>0) {
-            price += (isVeg ? 70 : 120);
+    public void addExtraToppings() {
+        if (!extraToppingsAdded) {
+            basePrice += (isVeg ? 70 : 120);
             bill.append("Extra Toppings Added: ").append(isVeg ? 70 : 120).append("\n");
             extraToppingsAdded = true;
         }
     }
 
-    public void addTakeaway(){
-        // your code goes here
-        if (!paperBagAdded&&price>0) {
-            price += 20;
+    public void addTakeaway() {
+        if (!paperBagAdded) {
+            basePrice += 20;
             bill.append("Paperbag Added: 20\n");
             paperBagAdded = true;
         }
     }
 
-    public String getBill(){
-        // your code goes here
-        if (price > 0) {
-            bill.append("Total Price: ").append(price).append("\n");
-        }
-        return this.bill.toString();
+    public int calculateTotalPrice() {
+        return basePrice;
+    }
+
+    public String getBill() {
+        bill.append("Total Price: ").append(calculateTotalPrice()).append("\n");
+        return bill.toString();
     }
 }
