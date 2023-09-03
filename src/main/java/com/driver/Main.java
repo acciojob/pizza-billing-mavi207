39350658efaf745bb25e76b7176e6198cc76e779
Mohammad Maviya Waseem Shaikh <maviya9830@gmail.com>
2023-09-03 19:11:17 +0530

    submit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,8 +5,6 @@ public class Main {
     DeluxePizza dp = new DeluxePizza(true);
     System.out.println(dp.getPrice());
     dp.addTakeaway();
-    dp.addExtraCheese();
-    dp.addTakeaway();
     System.out.println(dp.getBill());
 
     Pizza p = new Pizza(true);
